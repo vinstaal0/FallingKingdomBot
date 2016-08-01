a88ac5ebe976be39729a0f656555d6186515d111
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-01 10:41:27 +0200

    Update EnumHelp.java

--- src/main/java/me/Vinstaal0/Commands/Enums/EnumHelp.java
+++ src/main/java/me/Vinstaal0/Commands/Enums/EnumHelp.java
@@ -1,5 +1,10 @@
 package me.Vinstaal0.Commands.Enums;
 
 public static class EnumHelp {
+  
+  public static <T extends Enum<?>> T randomEnum(Class<T> clazz) {
+    int x = random.nextInt(clazz.getEnumConstants().length);
+    return clazz.getEnumConstants()[x];
+  }
 
 }
