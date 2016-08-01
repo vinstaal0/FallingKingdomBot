0fa6e5a7c21812bef960a0a8fc244f89c20e3476
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-01 15:27:29 +0200

    Update HelpCommand.java

--- src/main/java/me/Vinstaal0/Commands/HelpCommand.java
+++ src/main/java/me/Vinstaal0/Commands/HelpCommand.java
@@ -3,13 +3,16 @@ package me.Vinstaal0.Commands;
 public class HelpCommand implements CommandExecutor {
   
      @Command(aliases = {"!help"}, description = "Returns a list of commands")
-	  public void onCommand(Message message, String[] args) {
-		    message.reply("!GetRole");
-		    message.reply("!Ping");
-		    message.reply("!Playing");
-		    message.reply("!Role");
-		    message.reply("!Server");
-		    message.reply("!Help");
+	  public void onCommand(Message message, User user, String[] args) {
+		    user.sendMessage("!getrole");
+		    user.sendMessage("!ping");
+		    user.sendMessage("!playing <game>");
+		    user.sendMessage("!role");
+		    user.sendMessage("!server");
+		    user.sendMessage("!help");
+		    user.sendMessage("!rps <rock/paper/scissor>");
+		    user.sendMessage("!roll <Number>");
+		    user.sendMessage("!isunomadyet");
 	}
 
 }
