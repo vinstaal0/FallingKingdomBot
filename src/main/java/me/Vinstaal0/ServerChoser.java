1c124793b50de48a808bf9895f3ce2b83dfbdf59
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-02 16:30:43 +0200

    Update ServerChoser.java

--- src/main/java/me/Vinstaal0/ServerChoser.java
+++ src/main/java/me/Vinstaal0/ServerChoser.java
@@ -1,5 +1,29 @@
 package me.Vinstaal0;
 
 public class ServerChoser {
+  
+  public boolean isFallingKingdom(Server server) {
+      if (server.getId() == "FK ID") {
+          return true;
+      }   else {
+          return false;
+      }
+  }
+  
+  public boolean isVinstaal0Discord(Server server) {
+      if (server.getId() == "VS ID") {
+          return true;
+      }   else {
+          return false;
+      }
+  }
+  
+  public boolean isShootinHoops(Server server) {
+      if (server.getId() == "SH ID") {
+          return true;
+      }   else {
+          return false;
+      }
+  }
 
 }
