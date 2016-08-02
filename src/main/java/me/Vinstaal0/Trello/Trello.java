d09cb75ec4a27888bbe925939f0f397187b93f14
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-02 15:00:34 +0200

    Update Trello.java

--- src/main/java/me/Vinstaal0/Trello/Trello.java
+++ src/main/java/me/Vinstaal0/Trello/Trello.java
@@ -1,5 +1,10 @@
 package me.Vinstaal0.Trello;
 
 public class Trello {
+  
+  Trello trelloApi = new TrelloImpl(trellokey, trelloaccestoken);
+  
+  Board board = trelloApi.getBoard(trelloBoardForAddingCardsId);
+  List<TList> lists = board.fetchLists();
 
 }
