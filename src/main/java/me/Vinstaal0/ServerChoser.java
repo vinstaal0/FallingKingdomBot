be7534be9ea39a5ae636068ae1823f1869b537a2
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-03 10:56:38 +0200

    Update ServerChoser.java

--- src/main/java/me/Vinstaal0/ServerChoser.java
+++ src/main/java/me/Vinstaal0/ServerChoser.java
@@ -2,6 +2,32 @@ package me.Vinstaal0;
 
 public class ServerChoser {
   
+  public enum servers {
+     FALLINGKINGDOM("131434184857092096"),
+     VINSTAAL0DISCORD("109272327514005504"),
+     REALMGUILD("209719487572738049")
+    
+     private String id;
+    
+     private servers(String id) {
+          this.id = id;
+       }
+  }
+  
+  public String getServerName(Server server) {
+      
+      if (server.getId() == servers.FALLINGKINGDOM.getValue()) {
+         return servers.FALLINGKINGDOM.toString();
+      } else if (server.getId() == servers.VINSTAAL0DISCORD.getValue()) {
+         return servers.VINSTAAL0DISCORD.toString();
+      } else if (server.getId() == servers.REALMGUILD.getValue()) {
+         return servers.REALMGUILD.toString();
+      } else {
+         return null;
+      }
+    
+  };
+  
   public boolean isFallingKingdom(Server server) {
       if (server.getId() == "131434184857092096") {
           return true;
@@ -18,7 +44,7 @@ public class ServerChoser {
       }
   }
   
-  public boolean isShootinHoops(Server server) {
+  public boolean isRealmGuild(Server server) {
       if (server.getId() == "209719487572738049") {
           return true;
       }   else {
