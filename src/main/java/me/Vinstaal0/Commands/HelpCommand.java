e87e4ce404aa9a189701abd52cecf7c839c7573c
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-03 12:49:34 +0200

    Update HelpCommand.java

--- src/main/java/me/Vinstaal0/Commands/HelpCommand.java
+++ src/main/java/me/Vinstaal0/Commands/HelpCommand.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2016 Bastian Oppermann
+ * 
+ * Modified verion
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 package me.Vinstaal0.Commands;
 
 import de.btobastian.javacord.entities.User;
@@ -10,7 +19,10 @@ public class HelpCommand implements CommandExecutor {
 	// use: https://github.com/BtoBastian/JavacordBot/blob/master/src/main/java/de/btobastian/javacordbot/commands/HelpCommand.java
   
      @Command(aliases = {"!help", "!commands"}, description = "Shows this list")
-	  public void onCommand(Message message, User user, String[] args) {
+	  public void onCommand(Message message, User user) {
+	  	StringBuilder builder = new StringBuilder();
+	  	builder.append("```xml");
+	  	
 		    user.sendMessage("!getrole");
 		    user.sendMessage("!ping");
 		    user.sendMessage("!playing <game>");
