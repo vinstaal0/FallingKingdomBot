7cf68ec4826976a71cfd016264d95fad21aa9391
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-03 13:53:20 +0200

    Update HelpCommand.java

--- src/main/java/me/Vinstaal0/Commands/HelpCommand.java
+++ src/main/java/me/Vinstaal0/Commands/HelpCommand.java
@@ -19,19 +19,40 @@ public class HelpCommand implements CommandExecutor {
 	// use: https://github.com/BtoBastian/JavacordBot/blob/master/src/main/java/de/btobastian/javacordbot/commands/HelpCommand.java
   
      @Command(aliases = {"!help", "!commands"}, description = "Shows this list")
-	  public void onCommand(Message message, User user) {
-	  	StringBuilder builder = new StringBuilder();
-	  	builder.append("```xml");
+     public void onCommand(Message message, User user) {
+	StringBuilder builder = new StringBuilder();
+	builder.append("```xml");
+	for (CommandHandler.SimpleCommand simpleCommand : commandHandler.getCommands()) {
+		if (!simpleCommand.getCommandAnnotation().showInHelpPage()) {
+	  		continue; // skip command
+	  	}
+	  	builder.append("\n");
+	  	if (!simpleCommand.getCommandAnnotation().requiresMention()) {
+	  		// the default prefix only works if the command doesn't requir a mention
+	  		builder.append(commandHandler.getDefaultPrefix());
+	  	}
+	  	String usage = simpleCommand.getCommandAnnotation().usage();
+	  	if (usage.isEmpty()) { // no usage specified using first alias
+	  		usage = simplecommand.getCommandAnnotation().aliases()[0];
+	  	}
+	  	builder.append(usage);
+	  	String description = simpleCommand.getCommandAnnotation().description();
+	  	if (description != null) {
+	  		builder.append(" | ").append(description);
+	  	}
+	}
+	builder.append("\n```"); // end of xml code block
+	user.sendMessage(builder.toString();
 	  	
-		    user.sendMessage("!getrole");
-		    user.sendMessage("!ping");
-		    user.sendMessage("!playing <game>");
-		    user.sendMessage("!role");
-		    user.sendMessage("!server");
-		    user.sendMessage("!help");
-		    user.sendMessage("!rps <rock/paper/scissor>");
-		    user.sendMessage("!roll <Number>");
-		    user.sendMessage("!isunomadyet");
+		    //user.sendMessage("!getrole");
+		    //user.sendMessage("!ping");
+		    //user.sendMessage("!playing <game>");
+		    //user.sendMessage("!role");
+		    //user.sendMessage("!server");
+		    //user.sendMessage("!help");
+		    //user.sendMessage("!rps <rock/paper/scissor>");
+		    //user.sendMessage("!roll <Number>");
+		    //user.sendMessage("!isunomadyet");
 	}
 
 }
