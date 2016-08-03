594412b19b559a8e09641d61ed869cb49a84ed8b
Vinstaal0 <vincent-staaltjens@live.nl>
2016-08-03 14:10:09 +0200

    Update GetRoleCommand.java

--- src/main/java/me/Vinstaal0/Commands/GetRoleCommand.java
+++ src/main/java/me/Vinstaal0/Commands/GetRoleCommand.java
@@ -8,14 +8,11 @@ import de.btobastian.sdcf4j.CommandExecutor;
 
 public class GetRoleCommand implements CommandExecutor {
 	
-	@Command(aliases = {"!getrole"}, description = "prints a list of all roles + ID's")
+	@Command(aliases = {"!getrole"}, description = "prints a list of all roles + ID's", requiredPermission = "serverinfo")
 	public void onCommand(Server server, User user, Message message) {
 		
-		if (user.getRoles(server).contains(server.getRoleById("144529984088702978"))) {
-			System.out.println(server.getRoles());
-			
-			message.reply(server.getRoles().toString());
-		}
+		System.out.println(server.getRoles());
+		message.reply(server.getRoles().toString());
 			
 	}
 
